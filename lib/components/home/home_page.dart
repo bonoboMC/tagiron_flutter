75c3f4be9377f377e87e8ab3ef2b755b93dc81f9
bonoboMC <ete.koutaishi@gmail.com>
2020-09-22 17:44:20 +0900

    add provider pkg, make directory and tmp file

---
+++ lib/components/home/home_page.dart
@@ -0,0 +1,15 @@
+import '../import.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('title'),
+      ),
+      body: Center(
+        child: Text('Center Body'),
+      ),
+    );
+  }
+}
