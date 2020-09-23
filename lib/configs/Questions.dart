7fadb49703f74f1bf89c82475c81b3e70cd862db
bonoboMC <ete.koutaishi@gmail.com>
2020-09-23 13:24:21 +0900

    modifiy Questions class name

--- lib/configs/Questions.dart
+++ lib/configs/Questions.dart
@@ -1,15 +1,15 @@
-enum Question { WHERE_FIVE, WHERE_ZERO, WHERE_ONE_OR_TWO }
+enum Questions { WHERE_FIVE, WHERE_ZERO, WHERE_ONE_OR_TWO }
 
-extension QuestionExt on Question {
+extension QuestionExt on Questions {
   String get sentence {
     switch (this) {
-      case Question.WHERE_FIVE:
+      case Questions.WHERE_FIVE:
         return '5 はどこ';
         break;
-      case Question.WHERE_ZERO:
+      case Questions.WHERE_ZERO:
         return '0 はどこ？';
         break;
-      case Question.WHERE_ONE_OR_TWO:
+      case Questions.WHERE_ONE_OR_TWO:
         return '1 または 2 はどこ？(どちらか一つ選択)';
         break;
     }
